e4e8adca1692f84c21d2841af0868af5444687bd
jwonkyung <31203393+jwonkyung@users.noreply.github.com>
2018-10-23 17:51:14 +0900

    Update and rename 4_2_mountain_v3.cpp to 4_2_mountain.cpp

--- 4_2_mountain.cpp
+++ 4_2_mountain.cpp
@@ -1,6 +1,18 @@
 //4_2_mountain 
 // 참조: https://github.com/sungjk/algorithm/blob/master/src/pTp.cpp
 
+
+// 봉우리가 여러개인 산 모양을 출력한다. 산 모양은 그림과 같고 좌우 대칭이다.
+// 첫 번째 줄에 숫자를 입력 받는다. 숫자의 크기는 20보다 작은 자연수이다.
+// 출력 예의 형식으로 출력한다.
+
+// 입력
+// 3
+
+//출력
+// 1213121
+
+
 #include <iostream>
 #include <stdio.h> 
 using namespace std;
